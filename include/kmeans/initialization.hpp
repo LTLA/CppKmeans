f81e0cabc9069df29f30b852636290c6b9c2b602
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-07-18 18:24:43 -0700

    Added assorted options for initialization.

---
+++ include/kmeans/initialization.hpp
@@ -0,0 +1,160 @@
+#ifndef KMEANS_INITIALIZATION_HPP
+#define KMEANS_INITIALIZATION_HPP 
+
+#include <vector>
+#include <random>
+#include <cstdint>
+#include <iostream>
+
+/**
+ * @file initialization.hpp
+ *
+ * @brief Initialization methods to choose the starting centroids.
+ */
+
+namespace kmeans {
+
+/**
+ * @cond
+ */
+template<class ENGINE>
+double uniform01 (ENGINE& eng) {
+    // Stolen from Boost.
+    constexpr double factor = 1.0 / static_cast<double>((eng.max)()-(eng.min)());
+    double result;
+    do {
+        result = static_cast<double>(eng() - (eng.min)()) * factor;
+    } while (result == 1.0);
+    return result;
+}
+/**
+ * @endcond
+ */
+
+/**
+ * Implements the <b>k-means++</b> initialization described by Arthur and Vassilvitskii (2007).
+ * This approach involves the selection of starting points via iterations of weighted sampling, 
+ * where the sampling probability for each point is defined as the squared distance to the closest starting point that was chosen in any of the previous iterations.
+ * The aim is to obtain well-separated starting points to encourage the formation of suitable clusters.
+ *
+ * @tparam ENGINE A random number engine, e.g., `std::mt19937`.
+ *
+ * @param nd Number of dimensions.
+ * @param no Number of observations.
+ * @param data Pointer to an array where the dimensions are rows and the observations are columns.
+ * Data should be stored in column-major format.
+ * @param ncenters Number of centers to pick.
+ * @param eng An instance of a random number engine.
+ *
+ * @return A vector of indices for the observations that were selected as starting points.
+ * Note that the length may be less than `ncenters` if `ncenters > no` (in which case each observation is chosen as a starting point)
+ * or if there are duplicate observations (in which case only one of each set of duplicates is chosen).
+ *
+ * @see
+ * Arthur, D. and Vassilvitskii, S. (2007).
+ * k-means++: the advantages of careful seeding.
+ * _Proceedings of the eighteenth annual ACM-SIAM symposium on Discrete algorithms_, 1027-1035.
+ */
+template<class ENGINE>
+std::vector<int> weighted_initialization(int nd, int no, const double* data, int ncenters, ENGINE& eng) {
+    std::vector<int> targets(no);
+    std::vector<double> mindist(no, 1);
+    std::vector<double> cumulative(no);
+    std::vector<uint8_t> chosen(no);
+    std::vector<int> sofar;
+    sofar.reserve(ncenters);
+
+    for (int cen = 0; cen < ncenters; ++cen) {
+        int counter = 0;
+        if (!sofar.empty()) {
+            for (int obs = 0; obs < no; ++obs) {
+                if (!chosen[obs]) {
+                    targets[counter] = obs;
+                    double& curmin = mindist[counter];
+                    curmin = -1;
+
+                    for (auto so : sofar) {
+                        const double* acopy = data + obs * nd;
+                        const double* scopy = data + so * nd;
+                        double r2 = 0;
+                        for (int dim = 0; dim < nd; ++dim, ++acopy, ++scopy) {
+                            r2 += (*acopy - *scopy) * (*acopy - *scopy);
+                        }
+
+                        if (curmin < 0 || r2 < curmin) {
+                            curmin = r2;
+                        }
+                    }
+
+                    ++counter;
+                }
+            }
+        } else {
+            std::iota(targets.begin(), targets.end(), 0);
+            counter = no;
+        }
+
+        if (!counter) {
+            break;
+        }
+
+        cumulative[0] = mindist[0];
+        for (int i = 1; i < counter; ++i) {
+            cumulative[i] = cumulative[i-1] + mindist[i];
+        }
+
+        const double total = cumulative[counter-1];
+        if (total == 0) { // a.k.a. duplicates.
+            break;
+        }
+
+        const double chosen_weight = total * uniform01(eng);
+        auto chosen_pos = std::lower_bound(cumulative.begin(), cumulative.begin() + counter, chosen_weight);
+        auto chosen_id = targets[chosen_pos - cumulative.begin()];
+        chosen[chosen_id] = true;
+        sofar.push_back(chosen_id);
+    }
+
+    return sofar;
+}
+
+/**
+ * Implements a simple initialization of the starting points where random observations are sampled without replacement.
+ *
+ * @tparam ENGINE A random number engine, e.g., `std::mt19937`.
+ *
+ * @param nd Number of dimensions.
+ * @param no Number of observations.
+ * @param data Pointer to an array where the dimensions are rows and the observations are columns.
+ * Data should be stored in column-major format.
+ * @param ncenters Number of centers to pick.
+ * @param eng An instance of a random number engine.
+ *
+ * @return A vector of indices for the observations that were selected as starting points.
+ * Note that the length may be less than `ncenters` if `ncenters > no`, in which case each observation is chosen as a starting point.
+ */
+template<class ENGINE>
+std::vector<int> simple_initialization(int nd, int no, const double* data, int ncenters, ENGINE& eng) {
+    std::vector<int> sofar;
+
+    if (ncenters >= no) {
+        sofar.resize(no);
+        std::iota(sofar.begin(), sofar.end(), 0);
+    } else {
+        sofar.reserve(ncenters);
+        int traversed = 0;
+
+        while (sofar.size() < static_cast<size_t>(ncenters)) {
+            if (static_cast<double>(ncenters - sofar.size()) > static_cast<double>(no - traversed) * uniform01(eng)) {
+                sofar.push_back(traversed);
+            }
+            ++traversed;
+        }
+    }
+
+    return sofar;
+}
+
+}
+
+#endif
---
+++ tests/src/initialization.cpp
@@ -0,0 +1,162 @@
+#include "kmeans/initialization.hpp"
+#include <random>
+#include <vector>
+#include "TestCore.h"
+
+using InitializationTest = TestParamCore<std::tuple<int, int, int> >;
+
+TEST_P(InitializationTest, Simple) {
+    auto param = GetParam();
+    assemble(param);
+    auto ncenters = std::get<2>(param);
+
+    std::mt19937_64 rng(ncenters * 10);
+    auto output = kmeans::simple_initialization(nr, nc, data.data(), ncenters, rng);
+    auto copy = output;
+
+    EXPECT_EQ(output.size(), ncenters);
+    int last = -1;
+    std::sort(output.begin(), output.end());
+    for (auto o : output) {
+        EXPECT_TRUE(o > last); // no duplicates
+        EXPECT_TRUE(o < nc); // in range
+        last = o;
+    }
+
+    // Consistent results with the same initialization.
+    std::mt19937_64 rng2(ncenters * 10);
+    auto output2 = kmeans::simple_initialization(nr, nc, data.data(), ncenters, rng2);
+    EXPECT_EQ(copy, output2);
+
+    // Different results with a different seed (only works if num obs is reasonably larger than num centers).
+    std::mt19937_64 rng3(ncenters * 11);
+    auto output3 = kmeans::simple_initialization(nr, nc, data.data(), ncenters, rng3);
+    EXPECT_NE(copy, output3);
+}
+
+TEST_P(InitializationTest, Weighted) {
+    auto param = GetParam();
+    assemble(param);
+    auto ncenters = std::get<2>(param);
+
+    std::mt19937_64 rng(ncenters * 10);
+    auto output = kmeans::weighted_initialization(nr, nc, data.data(), ncenters, rng);
+    auto copy = output;
+
+    EXPECT_EQ(output.size(), ncenters);
+    int last = -1;
+    std::sort(output.begin(), output.end());
+    for (auto o : output) {
+        EXPECT_TRUE(o > last); // no duplicates
+        EXPECT_TRUE(o < nc); // in range
+        last = o;
+    }
+
+    // Consistent results with the same initialization.
+    std::mt19937_64 rng2(ncenters * 10);
+    auto output2 = kmeans::weighted_initialization(nr, nc, data.data(), ncenters, rng2);
+    EXPECT_EQ(copy, output2);
+    
+    // Different results with a different seed (only works if num obs is reasonably larger than num centers).
+    std::mt19937_64 rng3(ncenters * 11);
+    auto output3 = kmeans::weighted_initialization(nr, nc, data.data(), ncenters, rng3);
+    EXPECT_NE(copy, output3);
+}
+
+INSTANTIATE_TEST_CASE_P(
+    Initialization,
+    InitializationTest,
+    ::testing::Combine(
+        ::testing::Values(10, 20), // number of dimensions
+        ::testing::Values(20, 200, 2000), // number of observations
+        ::testing::Values(2, 5, 10) // number of clusters
+    )
+);
+
+using WeightedInitializationTest = TestParamCore<std::tuple<int, int, int> >;
+
+std::vector<int> one_to_n (int n) {
+    std::vector<int> output(n);
+    std::iota(output.begin(), output.end(), 0);
+    return output;
+}
+
+TEST_P(WeightedInitializationTest, Sanity) {
+    auto param = GetParam();
+    assemble(param);
+
+    // Duplicating the first 'nc' elements over and over again.
+    int ncenters = std::get<2>(param);
+
+    std::vector<int> choices(nc);
+    std::iota(choices.begin(), choices.begin() + ncenters, 0);
+
+    std::mt19937_64 rng(nc * 10);
+    auto dIt = data.begin() + ncenters * nr;
+    for (int c = ncenters; c < nc; ++c, dIt += nr) {
+        auto chosen = rng() % ncenters;
+        auto cIt = data.begin() + chosen * nr;
+        std::copy(cIt, cIt + nr, dIt);
+        choices[c] = chosen;
+    }
+
+    auto output = kmeans::weighted_initialization(nr, nc, data.data(), ncenters, rng);
+
+    // Expect one entry from each category.
+    EXPECT_EQ(output.size(), ncenters);
+    for (auto& o : output) {
+        o = choices[o];
+    }
+    std::sort(output.begin(), output.end());
+    EXPECT_EQ(one_to_n(ncenters), output);
+
+    // If more clusters are requested, we detect that only duplicates are available and we bail early.
+    auto output2 = kmeans::weighted_initialization(nr, nc, data.data(), ncenters + 1, rng);
+    EXPECT_EQ(output2.size(), ncenters);
+    for (auto& o : output2) {
+        o = choices[o];
+    }
+    std::sort(output2.begin(), output2.end());
+    EXPECT_EQ(one_to_n(ncenters), output2);
+}
+
+INSTANTIATE_TEST_CASE_P(
+    Initialization,
+    WeightedInitializationTest,
+    ::testing::Combine(
+        ::testing::Values(10, 20), // number of dimensions
+        ::testing::Values(20, 200, 2000), // number of observations
+        ::testing::Values(2, 5, 10) // number of clusters
+    )
+);
+
+using InitializationEdgeTest = TestParamCore<std::tuple<int, int> >;
+
+TEST_P(InitializationEdgeTest, TooManyClusters) {
+    auto param = GetParam();
+    assemble(param);
+    std::mt19937_64 rng(nc* 10);
+
+    auto soutput = kmeans::simple_initialization(nr, nc, data.data(), nc, rng);
+    EXPECT_EQ(soutput, one_to_n(nc));
+
+    auto soutput2 = kmeans::simple_initialization(nr, nc, data.data(), nc + 1, rng);
+    EXPECT_EQ(soutput2, one_to_n(nc));
+
+    auto woutput = kmeans::weighted_initialization(nr, nc, data.data(), nc, rng);
+    std::sort(woutput.begin(), woutput.end());
+    EXPECT_EQ(woutput, one_to_n(nc));
+
+    auto woutput2 = kmeans::weighted_initialization(nr, nc, data.data(), nc + 1, rng);
+    std::sort(woutput2.begin(), woutput2.end());
+    EXPECT_EQ(woutput2, one_to_n(nc));
+}
+
+INSTANTIATE_TEST_CASE_P(
+    Initialization,
+    InitializationEdgeTest,
+    ::testing::Combine(
+        ::testing::Values(10, 20), // number of dimensions
+        ::testing::Values(20, 50) // number of dimensions
+    )
+);
