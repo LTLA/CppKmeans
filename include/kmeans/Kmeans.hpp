a0efc161d06e12ffae73dc6af7a8eb98cf1664c6
Aaron Lun <lun.aaron@gene.com>
2021-07-19 13:18:56 -0700

    Templated input and output arrays, index type for observations. Throw if the index integer type is too small.

--- include/kmeans/Kmeans.hpp
+++ include/kmeans/Kmeans.hpp
@@ -21,12 +21,18 @@ namespace kmeans {
  * The cluster centroids themselves are chosen to minimize the sum of squared Euclidean distances from each observation to its assigned cluster.
  * This procedure involves some heuristics to choose a good initial set of centroids (see `weighted_initialization()` for details) 
  * and to converge to a local minimum (see `HartiganWong` for details).
+ *
+ * @tparam DATA_t Floating-point type for the data and centroids.
+ * @tparam CLUSTER_t Integer type for the cluster assignments.
+ * @tparam INDEX_t_t Integer type for the observation index.
+ * This should have a maximum positive value that is at least 50 times greater than the maximum expected number of observations.
  */
+template<typename DATA_t = double, typename CLUSTER_t = int, typename INDEX_t = int>
 class Kmeans {
     bool weighted_init = true;
     uint64_t seed = 5489u;
 
-    HartiganWong hw;
+    HartiganWong<DATA_t, INDEX_t, CLUSTER_t> hw;
 
 public:
     /** 
@@ -72,13 +78,13 @@ public:
     }
 
 private:
-    int initialize(int ndim, int nobs, const double* data, int ncenters, double* centers) {
-        std::vector<int> chosen;
+    CLUSTER_t initialize(int ndim, INDEX_t nobs, const DATA_t* data, CLUSTER_t ncenters, DATA_t* centers) {
+        std::vector<INDEX_t> chosen;
         std::mt19937_64 rng(seed);
         if (weighted_init) {
             chosen = weighted_initialization(ndim, nobs, data, ncenters, rng);
         } else {
-            chosen = simple_initialization(ndim, nobs, data, ncenters, rng);
+            chosen = simple_initialization(nobs, ncenters, rng);
         }
 
         for (auto c : chosen) {
@@ -107,7 +113,7 @@ public:
      * Note that the actual number of clusters may be less than `ncenters` in pathological cases - 
      * check the length of `HartiganWong::Details::sizes` and the value of `HartiganWong::Details::status`.
      */
-    HartiganWong::Details run(int ndim, int nobs, const double* data, int ncenters, double* centers, int* clusters) {
+    typename HartiganWong<DATA_t, INDEX_t, CLUSTER_t>::Details run(int ndim, INDEX_t nobs, const DATA_t* data, CLUSTER_t ncenters, DATA_t* centers, CLUSTER_t* clusters) {
         ncenters = initialize(ndim, nobs, data, ncenters, centers); 
         return hw.run(ndim, nobs, data, ncenters, centers, clusters);
     }
@@ -120,7 +126,7 @@ public:
         /**
          * @cond
          */
-        Results(int ndim, int nobs, int ncenters) : centers(ndim * ncenters), clusters(nobs) {}
+        Results(int ndim, INDEX_t nobs, CLUSTER_t ncenters) : centers(ndim * ncenters), clusters(nobs) {}
         /**
          * @endcond
          */
@@ -128,17 +134,17 @@ public:
         /**
          * A column-major `ndim`-by-`ncenters` array containing per-cluster centroid coordinates.
          */
-        std::vector<double> centers;
+        std::vector<DATA_t> centers;
 
         /**
          * An array of length `nobs` containing 0-indexed cluster assignments for each observation.
          */
-        std::vector<int> clusters;
+        std::vector<CLUSTER_t> clusters;
 
         /**
          * Further details from the Hartigan-Wong algorithm.
          */
-        HartiganWong::Details details;
+        typename HartiganWong<DATA_t, INDEX_t, CLUSTER_t>::Details details;
     };
 
     /**
@@ -151,7 +157,7 @@ public:
      * @return `centers` and `clusters` are filled, and a `Results` object is returned containing clustering statistics.
      * See `run()` for more details.
      */
-    Results run(int ndim, int nobs, const double* data, int ncenters) {
+    Results run(int ndim, INDEX_t nobs, const DATA_t* data, CLUSTER_t ncenters) {
         Results output(ndim, nobs, ncenters);
         ncenters = initialize(ndim, nobs, data, ncenters, output.centers.data()); 
         output.details = hw.run(ndim, nobs, data, ncenters, output.centers.data(), output.clusters.data());
