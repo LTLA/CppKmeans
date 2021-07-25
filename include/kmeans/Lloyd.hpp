e2a4f3491d3222dc0ea8a1662abfad9d0ea8a635
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-07-24 20:13:59 -0700

    Added an implementation of Lloyd's with NN support.

---
+++ include/kmeans/Lloyd.hpp
@@ -0,0 +1,163 @@
+#ifndef KMEANS_LLOYD_HPP
+#define KMEANS_LLOYD_HPP
+
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <cstdint>
+#include <stdexcept>
+#include <limits>
+
+#include "Details.hpp"
+#include "compute_wcss.hpp"
+#include "QuickSearch.hpp"
+#include "is_edge_case.hpp"
+
+/**
+ * @file Lloyd.hpp
+ *
+ * @brief Implements the Lloyd algorithm for k-means clustering.
+ */
+
+namespace kmeans {
+
+/**
+ * @brief Implements the Lloyd algorithm for k-means clustering.
+ *
+ * The Lloyd algorithm is the simplest k-means clustering algorithm,
+ * involving several iterations of batch assignments and center calculations.
+ * Specifically, we assign each observation to its closest cluster, and once all points are assigned, we recompute the cluster centroids.
+ * This is repeated until there are no reassignments or the maximum number of iterations is reached.
+ *
+ * @see
+ * Lloyd, S. P. (1982).  
+ * Least squares quantization in PCM.
+ * _IEEE Transactions on Information Theory_ 28, 128-137.
+ */
+template<typename DATA_t = double, typename CLUSTER_t = int, typename INDEX_t = int>
+class Lloyd {
+private:
+    int maxiter = 10;
+public:
+    /**
+     * @param m Maximum number of iterations.
+     * More iterations increase the opportunity for convergence at the cost of more computational time.
+     *
+     * @return A reference to this `Lloyd` object.
+     */
+    Lloyd& set_max_iterations(int m = 10) {
+        maxiter = m;
+        return *this;
+    }
+
+private:
+    void recompute_centroids(int ndim, INDEX_t nobs, const DATA_t* data, CLUSTER_t ncenters, DATA_t* centers, const CLUSTER_t* clusters, const std::vector<INDEX_t>& sizes) {
+        // Computing the average of each cluster.
+        std::fill(centers, centers + ncenters * ndim, 0);
+        for (INDEX_t obs = 0; obs < nobs; ++obs) {
+            auto copy = centers + clusters[obs] * ndim;
+            auto mine = data + obs * ndim;
+            for (int dim = 0; dim < ndim; ++dim, ++copy, ++mine) {
+                *copy += *mine;
+            }
+        }
+        
+        for (CLUSTER_t c = 0; c < ncenters; ++ c) {
+            if (sizes[c]) {
+                auto loc = centers + c * ndim;
+                for (int d = 0; d < ndim; ++d, ++loc) {
+                    *loc /= sizes[c];
+                }
+            }
+        }
+    }
+
+public:
+    /**
+     * @param ndim Number of dimensions.
+     * @param nobs Number of observations.
+     * @param[in] data Pointer to a `ndim`-by-`nobs` array where columns are observations and rows are dimensions. 
+     * Data should be stored in column-major order.
+     * @param ncenters Number of cluster centers.
+     * @param[in, out] centers Pointer to a `ndim`-by-`ncenters` array where columns are cluster centers and rows are dimensions. 
+     * On input, this should contain the initial centroid locations for each cluster.
+     * Data should be stored in column-major order.
+     * On output, this will contain the final centroid locations for each cluster.
+     * @param[out] clusters Pointer to an array of length `nobs`.
+     * On output, this will contain the cluster assignment for each observation.
+     *
+     * @return `centers` and `clusters` are filled, and a `Details` object is returned containing clustering statistics.
+     * If `ncenters > nobs`, only the first `nobs` columns of the `centers` array will be filled.
+     */
+    Details<DATA_t, INDEX_t> run(int ndim, INDEX_t nobs, const DATA_t* data, CLUSTER_t ncenters, DATA_t* centers, CLUSTER_t* clusters) {
+        int iter = 0, status = 0;
+        std::vector<INDEX_t> sizes(ncenters);
+
+        if (is_edge_case(nobs, ncenters, clusters, sizes, status)) {
+            if (ncenters) {
+                recompute_centroids(ndim, nobs, data, ncenters, centers, clusters, sizes);
+                return Details<DATA_t, INDEX_t>(
+                    std::move(sizes),
+                    compute_wcss(ndim, nobs, data, ncenters, centers, clusters),
+                    iter, 
+                    status
+                );
+            } else {
+                return Details<DATA_t, INDEX_t>(iter, status);
+            }
+        }
+
+        std::vector<CLUSTER_t> copy(nobs);
+        for (iter = 1; iter <= maxiter; ++iter) {
+            // Nearest-neighbor search to assign to the closest cluster.
+            // Note that we move the `updated` check outside of this loop
+            // so that, in the future, this is more easily parallelized.
+            QuickSearch<DATA_t, CLUSTER_t> index(ndim, ncenters, centers);
+            for (INDEX_t obs = 0; obs < nobs; ++obs) {
+                copy[obs] = index.find(data + obs * ndim);
+            }
+
+            bool updated = false;
+            for (INDEX_t obs = 0; obs < nobs; ++obs) {
+                if (copy[obs] != clusters[obs]) {
+                    updated = true;
+                    break;
+                }
+            }
+            if (!updated) {
+                break;
+            }
+            std::copy(copy.begin(), copy.end(), clusters);
+
+            // Counting the number in each cluster.
+            std::fill(sizes.begin(), sizes.end(), 0);
+            for (INDEX_t obs = 0; obs < nobs; ++obs) {
+                ++sizes[clusters[obs]];
+            }
+
+            for (CLUSTER_t c = 0; c < ncenters; ++ c) {
+                if (!sizes[c]) {
+                    status = 1;
+                    break;
+                }
+            }
+            
+            recompute_centroids(ndim, nobs, data, ncenters, centers, clusters, sizes);
+        }
+
+        if (iter == maxiter + 1) {
+            status = 2;
+        }
+
+        return Details<DATA_t, INDEX_t>(
+            std::move(sizes),
+            compute_wcss(ndim, nobs, data, ncenters, centers, clusters),
+            iter, 
+            status
+        );
+    }
+};
+
+}
+
+#endif
---
+++ tests/src/Lloyd.cpp
@@ -0,0 +1,137 @@
+#include "kmeans/Lloyd.hpp"
+#include <gtest/gtest.h>
+#include "TestCore.h"
+
+using LloydBasicTest = TestParamCore<std::tuple<int, int, int> >;
+
+TEST_P(LloydBasicTest, Sweep) {
+    auto param = GetParam();
+    assemble(param);
+    auto ncenters = std::get<2>(param);
+
+    auto centers = create_centers(data.data(), ncenters);
+    std::vector<int> clusters(nc);
+    auto hw = kmeans::Lloyd<>().run(nr, nc, data.data(), ncenters, centers.data(), clusters.data());
+
+    // Checking that there's the specified number of clusters, and that they're all non-empty.
+    std::vector<int> counts(ncenters);
+    for (auto c : clusters) {
+        EXPECT_TRUE(c >= 0 && c < ncenters);
+        ++counts[c];
+    }
+    EXPECT_EQ(counts, hw.sizes);
+    for (auto c : counts) {
+        EXPECT_TRUE(c > 0); 
+    }
+
+    EXPECT_TRUE(hw.iterations > 0);
+
+    // Checking that the WCSS calculations are correct.
+    const auto& wcss = hw.withinss;
+    for (size_t i = 0; i < ncenters; ++i) {
+        if (counts[i] > 1) {
+            EXPECT_TRUE(wcss[i] > 0);
+        } else {
+            EXPECT_EQ(wcss[i], 0);
+        }
+    }
+}
+
+INSTANTIATE_TEST_CASE_P(
+    Lloyd,
+    LloydBasicTest,
+    ::testing::Combine(
+        ::testing::Values(10, 20), // number of dimensions
+        ::testing::Values(20, 200, 2000), // number of observations 
+        ::testing::Values(2, 5, 10) // number of clusters 
+    )
+);
+
+using LloydConstantTest = TestParamCore<std::tuple<int, int> >;
+
+TEST_P(LloydConstantTest, TooMany) {
+    auto param = GetParam();
+    assemble(param);
+
+    std::vector<double> centers(data.size());
+    std::vector<int> clusters(nc);
+    auto hw = kmeans::Lloyd<>().run(nr, nc, data.data(), nc, centers.data(), clusters.data());
+
+    // Checking that the averages are just equal to the points.
+    EXPECT_EQ(data, centers);
+    EXPECT_EQ(hw.withinss, std::vector<double>(nc));
+    EXPECT_EQ(hw.sizes, std::vector<int>(nc, 1));
+    EXPECT_EQ(hw.iterations, 0);
+
+    std::vector<int> ref(nc);
+    std::iota(ref.begin(), ref.end(), 0);
+    EXPECT_EQ(ref, clusters);
+
+    centers.resize(nr * (nc + 1));
+    auto hw2 = kmeans::Lloyd<>().run(nr, nc, data.data(), nc + 1, centers.data(), clusters.data());
+    EXPECT_EQ(hw2.status, 3);
+    EXPECT_EQ(clusters, ref); // unchanged.
+    EXPECT_EQ(hw2.sizes[nc], 0); // last element is now zero.
+}
+
+TEST_P(LloydConstantTest, TooFew) {
+    auto param = GetParam();
+    assemble(param);
+
+    std::vector<double> centers(nr);
+    std::vector<int> clusters(nc);
+    auto hw = kmeans::Lloyd<>().run(nr, nc, data.data(), 1, centers.data(), clusters.data());
+
+    std::vector<double> averages(nr);
+    size_t i = 0;
+    for (auto d : data) {
+        averages[i] += d;
+        ++i;
+        i %= nr;
+    }
+    for (auto& a : averages) {
+        a /= nc;
+    }
+
+    EXPECT_EQ(centers, averages);
+    EXPECT_EQ(hw.iterations, 0);
+
+    // no points at all.
+    auto hw2 = kmeans::Lloyd<>().run(nr, nc, data.data(), 0, centers.data(), clusters.data());
+    EXPECT_EQ(hw2.status, 3);
+    EXPECT_EQ(hw2.sizes, std::vector<int>());
+}
+
+INSTANTIATE_TEST_CASE_P(
+    Lloyd,
+    LloydConstantTest,
+    ::testing::Combine(
+        ::testing::Values(10, 20), // number of dimensions
+        ::testing::Values(20, 200, 2000) // number of observations 
+    )
+);
+
+using LloydQuickNeighborTest = TestParamCore<std::tuple<int, int> >;
+
+TEST_P(LloydQuickNeighborTest, Sweep) {
+    auto param = GetParam();
+    assemble(param);
+
+    // Quick and dirty check to verify that we do get back the right identity.
+    kmeans::QuickSearch index(nr, nc, data.data()); 
+    for (size_t c = 0; c < nc; ++c) {
+        auto best = index.find(data.data() + c * nr);
+        EXPECT_EQ(c, best);
+    }
+}
+
+INSTANTIATE_TEST_CASE_P(
+    Lloyd,
+    LloydQuickNeighborTest,
+    ::testing::Combine(
+        ::testing::Values(10, 20), // number of dimensions
+        ::testing::Values(2, 10, 50) // number of observations 
+    )
+);
+
+
