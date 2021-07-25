a5a8435506b523343c5ad2ef1b2bbc1c6c16b0e1
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-07-24 20:23:37 -0700

    Added checks against stats::kmeans' Lloyd implementation.

--- tests/R/run.R
+++ tests/R/run.R
@@ -12,26 +12,55 @@ if (!file.exists("kmeans")) {
 sourceCpp("test.cpp")
 
 library(testthat)
-for (nc in c(20, 50, 100, 500, 2000)) {
-    for (nr in c(2, 10, 20)) {
-        for (k in c(2, 5, 10)) {
-            cat("NR = ", nr, ", NC = ", nc, ", k = " , k, "\n", sep="")
 
-            # Generating some data.
-            set.seed(nc / nr + k)
-            mat <- matrix(rnorm(nr * nc), ncol=nc)
-            centers <- sample(nc, k)
-            init <- mat[,centers]
-            
-            out <- run_kmeans(mat, init)
-            ref <- kmeans(t(mat), centers=t(init))
+test_that("Hartigan-Wong works correctly", {
+    for (nc in c(20, 50, 100, 500, 2000)) {
+        for (nr in c(2, 10, 20)) {
+            for (k in c(2, 5, 10)) {
+                cat("NR = ", nr, ", NC = ", nc, ", k = " , k, "\n", sep="")
 
-            expect_equal(out$centers, unname(t(ref$centers)))
-            expect_identical(out$clusters + 1L, ref$cluster)
-            expect_identical(out$size, ref$size)
-            expect_equal(out$wcss, ref$withinss)
-            expect_identical(out$status, ref$ifault)
-            expect_identical(out$iterations, ref$iter)
+                # Generating some data.
+                set.seed(nc / nr + k)
+                mat <- matrix(rnorm(nr * nc), ncol=nc)
+                centers <- sample(nc, k)
+                init <- mat[,centers]
+                
+                out <- run_kmeans_hw(mat, init)
+                ref <- kmeans(t(mat), centers=t(init))
+
+                expect_equal(out$centers, unname(t(ref$centers)))
+                expect_identical(out$clusters + 1L, ref$cluster)
+                expect_identical(out$size, ref$size)
+                expect_equal(out$wcss, ref$withinss)
+                expect_identical(out$status, ref$ifault)
+                expect_identical(out$iterations, ref$iter)
+            }
         }
     }
-}
+})
+
+test_that("Lloyd works correctly", {
+    for (nc in c(20, 50, 100, 500, 2000)) {
+        for (nr in c(2, 10, 20)) {
+            for (k in c(2, 5, 10)) {
+                cat("NR = ", nr, ", NC = ", nc, ", k = " , k, "\n", sep="")
+
+                # Generating some data.
+                set.seed(nc / nr + k)
+                mat <- matrix(rnorm(nr * nc), ncol=nc)
+                centers <- sample(nc, k)
+                init <- mat[,centers]
+                
+                out <- run_kmeans_lloyd(mat, init)
+                ref <- suppressWarnings(kmeans(t(mat), centers=t(init), algorithm="Lloyd"))
+
+                expect_equal(out$centers, unname(t(ref$centers)))
+                expect_identical(out$clusters + 1L, ref$cluster)
+                expect_identical(out$size, ref$size)
+                expect_equal(out$wcss, ref$withinss)
+                expect_identical(out$iterations, ref$iter)
+            }
+        }
+    }
+})
+
--- tests/R/test.cpp
+++ tests/R/test.cpp
@@ -1,13 +1,29 @@
 #include "Rcpp.h"
 #include "kmeans/HartiganWong.hpp"
+#include "kmeans/Lloyd.hpp"
 
 // [[Rcpp::plugins(cpp17)]]
 
 // [[Rcpp::export(rng=false)]]
-Rcpp::List run_kmeans(Rcpp::NumericMatrix x, Rcpp::NumericMatrix init) {
+Rcpp::List run_kmeans_hw(Rcpp::NumericMatrix x, Rcpp::NumericMatrix init) {
     Rcpp::NumericMatrix output = Rcpp::clone(init);
     Rcpp::IntegerVector clusters(x.ncol());
-    auto hw = kmeans::HartiganWong().run(x.nrow(), x.ncol(), x.begin(), output.ncol(), output.begin(), clusters.begin());
+    auto hw = kmeans::HartiganWong<>().run(x.nrow(), x.ncol(), x.begin(), output.ncol(), output.begin(), clusters.begin());
+    return Rcpp::List::create(
+        Rcpp::Named("centers") = output, 
+        Rcpp::Named("clusters") = clusters,
+        Rcpp::Named("size") = Rcpp::wrap(hw.sizes),
+        Rcpp::Named("wcss") = Rcpp::wrap(hw.withinss),
+        Rcpp::Named("status") = hw.status,
+        Rcpp::Named("iterations") = hw.iterations
+    );
+}
+
+// [[Rcpp::export(rng=false)]]
+Rcpp::List run_kmeans_lloyd(Rcpp::NumericMatrix x, Rcpp::NumericMatrix init) {
+    Rcpp::NumericMatrix output = Rcpp::clone(init);
+    Rcpp::IntegerVector clusters(x.ncol());
+    auto hw = kmeans::Lloyd<>().run(x.nrow(), x.ncol(), x.begin(), output.ncol(), output.begin(), clusters.begin());
     return Rcpp::List::create(
         Rcpp::Named("centers") = output, 
         Rcpp::Named("clusters") = clusters,
