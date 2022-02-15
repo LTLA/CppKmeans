cec768aac3d491ed9bdd75d58f6cfff3af71680d
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-02-15 20:50:58 +0000

    Deploying to gh-pages from @ LTLA/CppKmeans@9323ce46574d87171498b71cc871db14770a0fe0 🚀

--- docs/search/all_1.js
+++ docs/search/all_1.js
@@ -1,8 +1,7 @@
 var searchData=
 [
   ['c_2b_2b_20library_20for_20k_2dmeans_0',['C++ library for k-means',['../index.html',1,'']]],
-  ['cap_1',['cap',['../structkmeans_1_1InitializePCAPartition_1_1Defaults.html#a9341bb83d5c8cc771c78cf0c7c585010',1,'kmeans::InitializePCAPartition::Defaults']]],
-  ['centers_2',['centers',['../structkmeans_1_1Kmeans_1_1Results.html#a6700ec6cc103a10953038d8aac43ab57',1,'kmeans::Kmeans::Results']]],
-  ['clusters_3',['clusters',['../structkmeans_1_1Kmeans_1_1Results.html#a55cd96f75f297d97aa94ad5a509bc3dc',1,'kmeans::Kmeans::Results']]],
-  ['convergence_5fhistory_4',['convergence_history',['../structkmeans_1_1MiniBatch_1_1Defaults.html#a269677460d39d3688f6de0d42d7e1061',1,'kmeans::MiniBatch::Defaults']]]
+  ['centers_1',['centers',['../structkmeans_1_1Kmeans_1_1Results.html#a6700ec6cc103a10953038d8aac43ab57',1,'kmeans::Kmeans::Results']]],
+  ['clusters_2',['clusters',['../structkmeans_1_1Kmeans_1_1Results.html#a55cd96f75f297d97aa94ad5a509bc3dc',1,'kmeans::Kmeans::Results']]],
+  ['convergence_5fhistory_3',['convergence_history',['../structkmeans_1_1MiniBatch_1_1Defaults.html#a269677460d39d3688f6de0d42d7e1061',1,'kmeans::MiniBatch::Defaults']]]
 ];
--- docs/search/variables_1.js
+++ docs/search/variables_1.js
@@ -1,7 +1,6 @@
 var searchData=
 [
-  ['cap_0',['cap',['../structkmeans_1_1InitializePCAPartition_1_1Defaults.html#a9341bb83d5c8cc771c78cf0c7c585010',1,'kmeans::InitializePCAPartition::Defaults']]],
-  ['centers_1',['centers',['../structkmeans_1_1Kmeans_1_1Results.html#a6700ec6cc103a10953038d8aac43ab57',1,'kmeans::Kmeans::Results']]],
-  ['clusters_2',['clusters',['../structkmeans_1_1Kmeans_1_1Results.html#a55cd96f75f297d97aa94ad5a509bc3dc',1,'kmeans::Kmeans::Results']]],
-  ['convergence_5fhistory_3',['convergence_history',['../structkmeans_1_1MiniBatch_1_1Defaults.html#a269677460d39d3688f6de0d42d7e1061',1,'kmeans::MiniBatch::Defaults']]]
+  ['centers_0',['centers',['../structkmeans_1_1Kmeans_1_1Results.html#a6700ec6cc103a10953038d8aac43ab57',1,'kmeans::Kmeans::Results']]],
+  ['clusters_1',['clusters',['../structkmeans_1_1Kmeans_1_1Results.html#a55cd96f75f297d97aa94ad5a509bc3dc',1,'kmeans::Kmeans::Results']]],
+  ['convergence_5fhistory_2',['convergence_history',['../structkmeans_1_1MiniBatch_1_1Defaults.html#a269677460d39d3688f6de0d42d7e1061',1,'kmeans::MiniBatch::Defaults']]]
 ];
