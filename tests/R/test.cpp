9a288ffb68d0e6e84ff39b407b2fb6d898629d51
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-06-06 07:54:28 -0700

    Got the tests to pass?

--- tests/R/test.cpp
+++ tests/R/test.cpp
@@ -1,6 +1,6 @@
 #include "Rcpp.h"
-#include "kmeans/HartiganWong.hpp"
-#include "kmeans/Lloyd.hpp"
+#include "kmeans/RefineHartiganWong.hpp"
+#include "kmeans/RefineLloyd.hpp"
 
 // [[Rcpp::plugins(cpp17)]]
 
@@ -8,14 +8,17 @@
 Rcpp::List run_kmeans_hw(Rcpp::NumericMatrix x, Rcpp::NumericMatrix init) {
     Rcpp::NumericMatrix output = Rcpp::clone(init);
     Rcpp::IntegerVector clusters(x.ncol());
-    auto hw = kmeans::HartiganWong<>().run(x.nrow(), x.ncol(), x.begin(), output.ncol(), output.begin(), clusters.begin());
+
+    kmeans::RefineHartiganWong hw;
+    kmeans::SimpleMatrix<double, int> mat(x.nrow(), x.ncol(), x.begin());
+    auto res = hw.run(mat, output.ncol(), output.begin(), clusters.begin());
+
     return Rcpp::List::create(
         Rcpp::Named("centers") = output, 
         Rcpp::Named("clusters") = clusters,
-        Rcpp::Named("size") = Rcpp::wrap(hw.sizes),
-        Rcpp::Named("wcss") = Rcpp::wrap(hw.withinss),
-        Rcpp::Named("status") = hw.status,
-        Rcpp::Named("iterations") = hw.iterations
+        Rcpp::Named("size") = Rcpp::wrap(res.sizes),
+        Rcpp::Named("status") = res.status,
+        Rcpp::Named("iterations") = res.iterations
     );
 }
 
@@ -23,13 +26,16 @@ Rcpp::List run_kmeans_hw(Rcpp::NumericMatrix x, Rcpp::NumericMatrix init) {
 Rcpp::List run_kmeans_lloyd(Rcpp::NumericMatrix x, Rcpp::NumericMatrix init) {
     Rcpp::NumericMatrix output = Rcpp::clone(init);
     Rcpp::IntegerVector clusters(x.ncol());
-    auto hw = kmeans::Lloyd<>().run(x.nrow(), x.ncol(), x.begin(), output.ncol(), output.begin(), clusters.begin());
+
+    kmeans::RefineLloyd ll;
+    kmeans::SimpleMatrix<double, int> mat(x.nrow(), x.ncol(), x.begin());
+    auto res = ll.run(mat, output.ncol(), output.begin(), clusters.begin());
+
     return Rcpp::List::create(
         Rcpp::Named("centers") = output, 
         Rcpp::Named("clusters") = clusters,
-        Rcpp::Named("size") = Rcpp::wrap(hw.sizes),
-        Rcpp::Named("wcss") = Rcpp::wrap(hw.withinss),
-        Rcpp::Named("status") = hw.status,
-        Rcpp::Named("iterations") = hw.iterations
+        Rcpp::Named("size") = Rcpp::wrap(res.sizes),
+        Rcpp::Named("status") = res.status,
+        Rcpp::Named("iterations") = res.iterations
     );
 }
