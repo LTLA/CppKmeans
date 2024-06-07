0b751e21db8b9e2d79f5b0af4ba7f7be9813c797
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-06-07 14:02:18 +0000

    Deploying to gh-pages from @ LTLA/CppKmeans@02d87e416757fdddb15f631de2abf8d958cb799c 🚀

--- docs/search/all_c.js
+++ docs/search/all_c.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['seed_0',['seed',['../structkmeans_1_1InitializeKmeansppOptions.html#acf0d2057e8792438afe8bf8a82d03307',1,'kmeans::InitializeKmeansppOptions::seed'],['../structkmeans_1_1InitializePcaPartitionOptions.html#a966e01854b00ca80d8ffbc273888f096',1,'kmeans::InitializePcaPartitionOptions::seed'],['../structkmeans_1_1InitializeRandomOptions.html#a90f47cd2ef6c910423f90cc4f5678602',1,'kmeans::InitializeRandomOptions::seed'],['../structkmeans_1_1RefineMiniBatchOptions.html#aa7a1dda3a142c5f2b4221af7fc8f05b9',1,'kmeans::RefineMiniBatchOptions::seed']]],
-  ['simplematrix_1',['SimpleMatrix',['../classkmeans_1_1SimpleMatrix.html',1,'kmeans::SimpleMatrix&lt; Data_, Index_ &gt;'],['../classkmeans_1_1SimpleMatrix.html#a52c6c92575e8e6b7e2504b4078e2c0f5',1,'kmeans::SimpleMatrix::SimpleMatrix()']]],
+  ['simplematrix_1',['simplematrix',['../classkmeans_1_1SimpleMatrix.html',1,'kmeans::SimpleMatrix&lt; Data_, Index_ &gt;'],['../classkmeans_1_1SimpleMatrix.html#a52c6c92575e8e6b7e2504b4078e2c0f5',1,'kmeans::SimpleMatrix::SimpleMatrix()']]],
   ['simplematrix_2ehpp_2',['SimpleMatrix.hpp',['../SimpleMatrix_8hpp.html',1,'']]],
   ['size_5fadjustment_3',['size_adjustment',['../structkmeans_1_1InitializePcaPartitionOptions.html#a68075e7854df8e74f0a7eaaa1b95b1ae',1,'kmeans::InitializePcaPartitionOptions']]],
   ['sizes_4',['sizes',['../structkmeans_1_1Details.html#a4be54a935f902c228f0136875dad36be',1,'kmeans::Details']]],
